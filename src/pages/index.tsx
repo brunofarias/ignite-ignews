aff9bd4e842424d7724551f6df06a0a2e0a33421
Bruno Farias <begfarias@gmail.com>
2021-03-26 19:51:30 -0300

    :rocket: feat: Page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
 import Head from 'next/head';
+import styles from '../styles/pages/home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>ig.news</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world
+            <p>
+              Get access to all the publication <br />
+              <span>for $9.90 month</span>
+            </p>
+          </h1>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
