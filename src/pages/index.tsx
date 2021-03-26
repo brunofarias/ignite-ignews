ec519fef794c06c02f48f6c7fe56e7bdf626737a
Bruno Farias <begfarias@gmail.com>
2021-03-26 20:01:43 -0300

    :rocket: feat: Component subscribebutton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
 import styles from '../styles/pages/home.module.scss';
 
 export default function Home() {
@@ -11,13 +13,9 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
-          <h1>
-            News about the <span>React</span> world
-            <p>
-              Get access to all the publication <br />
-              <span>for $9.90 month</span>
-            </p>
-          </h1>
+          <h1>News about the <span>React</span> world</h1>
+          <p>Get access to all the publication <br/><span>for $9.90 month</span></p>
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
