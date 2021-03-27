170094f53b4651105b50390bc2bbcbeda648f408
Bruno Farias <begfarias@gmail.com>
2021-03-27 17:37:03 -0300

    :rocket: feat: Save user in FaunaDB

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,27 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Bruno Farias
+      {session.user.name}
       <FiX color="#b8c6f7" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sing in with GitHub
     </button>
